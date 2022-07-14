dd4b00b86a10983b74f888a12e13d448923087ef
Ashfame <mail@ashfame.com>
2022-07-14 17:34:42 +0400

    expand settings section to have overrides field

--- wp/plugin.php
+++ wp/plugin.php
@@ -14,8 +14,15 @@
  */
 function a8c_orbit_chatterbox_settings() {
     register_setting( 'discussion', 'a8c_orbit_chatterbox_settings', array(
-		'type' => 'string',
-		'sanitize_callback' => 'sanitize_text_field',
+		'type' => 'array',
+		'sanitize_callback' => function( $value ) {
+			$value['default_matrix_room'] = sanitize_text_field( $value['default_matrix_room'] );
+			foreach ( $value['overrides'] as $index => $override ) {
+				$value['overrides'][$index]['room'] = sanitize_text_field( $override['room'] );
+				$value['overrides'][$index]['pages'] = array_filter( array_map( 'absint', $override['pages'] ) );
+			}
+			return $value;
+		},
 		'default' => NULL,
 	) );
 
@@ -33,12 +40,60 @@ function a8c_orbit_chatterbox_settings() {
 
     // register a new field in the "a8c_orbit_chatterbox_section" section, on the "discussion" page
     add_settings_field(
-        'matrix-room-default',
-        'Matrix room (Default)',
+        'default-matrix-room',
+        'Default Matrix room',
 		function() {
-			$setting = get_option('a8c_orbit_chatterbox_settings');
+			$settings = get_option( 'a8c_orbit_chatterbox_settings' );
+
+			// cleanup, since we are hacking Settings API to work for ourselves in a certain way
+			$modified = false;
+			foreach ( $settings['overrides'] as $key => $override ) {
+				if ( empty( trim( $override['room'] ) ) || empty( $override['pages'] ) ) {
+					unset( $settings['overrides'][$key] );
+					$modified = true;
+				}
+			}
+			if ( $modified ) {
+				update_option( 'a8c_orbit_chatterbox_settings', $settings );
+			}
+
 			?>
-			<input type="text" name="a8c_orbit_chatterbox_settings" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>" class="regular-text">
+			<input type="text" name="a8c_orbit_chatterbox_settings[default_matrix_room]" value="<?php echo isset( $settings['default_matrix_room'] ) ? esc_attr( $settings['default_matrix_room'] ) : ''; ?>" class="regular-text">
+			<p class="description">Enter default matrix room to embed on all pages on your website.</p>
+			<p class="description">If you only want to embed Matrix chat room on certain pages, leave this field empty and only fill out overrides below.</p>
+			<h3>Overrides</h3>
+			<?php $key = 0; ?>
+			<?php foreach ( $settings['overrides'] as $key => $override ) { ?>
+				<div class="a8c-orbit-chatterbox-overrides">
+					<p class="description">Embed this room
+						<input type="text" name="a8c_orbit_chatterbox_settings[overrides][<?php echo $key ?>][room]" value="<?php echo isset( $override['room'] ) ? esc_attr( $override['room'] ) : ''; ?>" class="regular-text">
+						on these pages:
+
+						<select multiple name="a8c_orbit_chatterbox_settings[overrides][<?php echo $key ?>][pages][]">
+						<?php foreach ( get_pages() as $page ) { ?>
+								<option value="<?php echo $page->ID ?>" <?php echo in_array( $page->ID, $override['pages'] ) ? 'selected' : ''; ?>><?php echo $page->post_title ?></option>
+							<?php } ?>
+						</select>
+					</p>
+				</div>
+				<br />
+			<?php } ?>
+			<?php
+			// Show additional set of fields to add new entry for overrides
+			$key++;
+			?>
+			<div class="a8c-orbit-chatterbox-overrides">
+				<p class="description">Embed this room
+					<input type="text" name="a8c_orbit_chatterbox_settings[overrides][<?php echo $key ?>][room]" value="" class="regular-text">
+					on these pages:
+
+					<select multiple name="a8c_orbit_chatterbox_settings[overrides][<?php echo $key ?>][pages][]">
+						<?php foreach ( get_pages() as $page ) { ?>
+							<option value="<?php echo $page->ID ?>"><?php echo $page->post_title ?></option>
+						<?php } ?>
+					</select>
+				</p>
+			</div>
 			<?php
 		},
         'discussion',
