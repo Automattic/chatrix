4a9bf07e39c7bf8cdee1622851b6c87c4454b484
Paulo Pinto <paulo.pinto@automattic.com>
2022-07-15 15:38:10 +0100

    Implement login with password

--- src/viewmodels/PasswordLoginViewModel.ts
+++ src/viewmodels/PasswordLoginViewModel.ts
@@ -14,12 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { ViewModel } from "hydrogen-view-sdk";
+import {Client, ViewModel, LoadStatus} from "hydrogen-view-sdk";
 import "hydrogen-view-sdk/style.css";
 import {IChatterboxConfig} from "../types/IChatterboxConfig";
 
 export class PasswordLoginViewModel extends ViewModel {
     private readonly _config: IChatterboxConfig;
+    private _client: typeof Client;
     private _username: string;
     private _password: string;
     private _errorMessage: string;
@@ -27,6 +28,7 @@ export class PasswordLoginViewModel extends ViewModel {
     constructor(options) {
         super(options);
         this._config = options.config;
+        this._client = options.client;
         this._username = this._config.username;
         this._password = this._config.password;
         this._errorMessage = "";
@@ -49,9 +51,35 @@ export class PasswordLoginViewModel extends ViewModel {
         this.emitChange("errorMessage");
     }
 
-    // @ts-ignore remove this line once method is implemented
     async login(username: string, password: string) {
         this._showError("")
-        // TODO: Implement me
+
+        const loginOptions = await this._client.queryLogin(this._config.homeserver).result;
+        this._client.startWithLogin(loginOptions.password(username, password));
+
+        const loadStatus = this._client.loadStatus;
+        const handle = loadStatus.waitFor(status => status !== LoadStatus.Login);
+        await handle.promise;
+
+        const status = loadStatus.get();
+        if (status === LoadStatus.LoginFailed) {
+            let error = "";
+            switch (this._client.loginFailure) {
+                case "Credentials":
+                    error = this.i18n`Your username and/or password don't seem to be correct.`;
+                    break;
+                case "Connection":
+                    error = this.i18n`Can't connect to ${this._config.homeserver}.`;
+                    break;
+                default:
+                    error = this.i18n`Something went wrong while checking your login and password.`;
+                    break;
+            }
+
+            this._showError(error);
+            return;
+        }
+
+        this.navigation.push("timeline");
     }
 }
