a191dc35c88f9696c15a9f039e09f48e27f66029
Alex Kirk <akirk@users.noreply.github.com>
2022-08-05 13:56:02 +0200

    Add some make.wordpress.org HTML

--- src/ui/views/FooterView.ts
+++ src/ui/views/FooterView.ts
@@ -15,7 +15,6 @@ limitations under the License.
 */
 
 import { TemplateView } from "hydrogen-view-sdk";
-import chatterboxLogo from "../res/chat-bubbles.svg";
 import { FooterViewModel } from "../../viewmodels/FooterViewModel";
 
 export class FooterView extends TemplateView<FooterViewModel> {
@@ -24,28 +23,6 @@ export class FooterView extends TemplateView<FooterViewModel> {
     }
 
     render(t, vm) {
-        return t.div({ className: "FooterView" }, [
-            t.div([
-                t.img({ src: chatterboxLogo, className: "FooterView_logo" }),
-                t.a(
-                    {
-                        className: "FooterView_chatterbox-branding",
-                        href: vm.chatterboxLink,
-                        target: "_top",
-                        rel: "noopener"
-                    },
-                    "Chatterbox"
-                ),
-            ]),
-            t.a(
-                {
-                    className: "FooterView_matrix-branding",
-                    href: vm.matrixLink,
-                    target: "_top",
-                    rel: "noopener"
-                },
-                "Powered by Matrix"
-            ),
-        ]);
+        return t.div({ className: "FooterView" } );
     }
 }
