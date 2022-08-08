a4ae3cf96a1ee6981beb798dc5ab1c1cb9e069b7
Paulo Pinto <paulo.pinto@automattic.com>
2022-08-08 14:20:05 +0100

    Add basic WordPress plugin

---
+++ wordpress/plugin/chatrix.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Chatrix
+ * Description: Embed the Chatrix Matrix client into WordPress pages.
+ * Author: Automattic
+ * Author URI: https://automattic.com/
+ * Plugin URI: https://github.com/Automattic/chatrix
+ * Version: 1.0
+ */
+namespace Chatrix;
+
+add_action( 'wp_loaded', function() {
+
+} );
