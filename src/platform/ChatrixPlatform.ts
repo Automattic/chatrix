f883bb90d338a3c360edea10e43416d368ea95c6
Ashfame <mail@ashfame.com>
2022-08-23 16:06:00 +0400

    cleanup other users' sessions based on active backendUserId

--- src/platform/ChatrixPlatform.ts
+++ src/platform/ChatrixPlatform.ts
@@ -20,23 +20,23 @@ import { SessionInfoStorage } from "./SessionInfoStorage";
 export class ChatrixPlatform extends Platform {
     constructor(options, instanceId: string | null, backendUserId: string | null) {
         super(options);
-        let sessionName = "hydrogen_sessions_v1"
+        let sessionPrefix = "hydrogen_sessions_v1";
+        let sessionName = sessionPrefix;
         if (instanceId && instanceId !== "") {
             sessionName = `${sessionName}_${instanceId}`;
         }
         if (backendUserId && backendUserId !== "") {
             sessionName = `${sessionName}_${backendUserId}`;
+            this.cleanupSessions(sessionPrefix,backendUserId);
         }
 
         this.sessionInfoStorage = new SessionInfoStorage(sessionName);
-
-        this.cleanupSessions(sessionName);
     }
 
-    cleanupSessions(activeSessionName: string) {
+    cleanupSessions(sessionPrefix: string, backendUserId: string) {
         for (let i=0; i<localStorage.length; i++) {
             let key = localStorage.key(i);
-            if (!key.startsWith("hydrogen_sessions_v1") || key === activeSessionName) {
+            if (!key.startsWith(sessionPrefix) || key.endsWith(backendUserId)) {
                 continue;
             }
             this.invalidateSession(
