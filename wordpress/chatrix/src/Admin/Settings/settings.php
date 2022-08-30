2a8faf348f03f8a8224674d28eca9473feb55460
Paulo Pinto <paulo.pinto@automattic.com>
2022-08-30 15:46:54 +0100

    Remove React
    We're no longer building the settings page in JS.

--- wordpress/chatrix/src/Admin/Settings/settings.php
+++ wordpress/chatrix/src/Admin/Settings/settings.php
@@ -2,28 +2,6 @@
 
 namespace Automattic\Chatrix\Admin\Settings;
 
-use function Automattic\Chatrix\Admin\admin_asset_url;
-
-function scripts() {
-	wp_enqueue_style(
-		'chatrix-style',
-		admin_asset_url( 'settings.css' ),
-		array(),
-		'1.0'
-	);
-
-	wp_enqueue_script(
-		'chatrix-settings-js',
-		admin_asset_url( 'settings.js' ),
-		array(
-			'wp-element',
-			'wp-i18n',
-		),
-		'1.0',
-		true
-	);
-}
-
 function main() {
 	add_options_page(
 		'Chatrix',
