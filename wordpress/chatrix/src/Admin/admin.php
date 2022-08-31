b0d4187dfed7abfde6c3525d1ed60a880729b965
Paulo Pinto <paulo.pinto@automattic.com>
2022-08-31 15:30:25 +0100

    Remove settings-in-json

--- wordpress/chatrix/src/Admin/admin.php
+++ wordpress/chatrix/src/Admin/admin.php
@@ -5,7 +5,4 @@ namespace Automattic\Chatrix\Admin;
 function main() {
 	add_action( 'admin_init', 'Automattic\Chatrix\Admin\Settings\init' );
 	add_action( 'admin_menu', 'Automattic\Chatrix\Admin\Settings\menu' );
-
-	// TODO: Remove following line once new settings page is fully implemented.
-	add_action( 'admin_init', 'Automattic\Chatrix\Admin\Settings\settings_in_json' );
 }
