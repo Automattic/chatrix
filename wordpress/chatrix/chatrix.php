9475e9279ed5e60ffd148cebecedb673c26ad4cf
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-01 14:44:31 +0100

    Only run when admin

--- wordpress/chatrix/chatrix.php
+++ wordpress/chatrix/chatrix.php
@@ -15,17 +15,17 @@ if ( ! function_exists( 'Automattic\Chatrix\main' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 }
 
-// Link to the plugin settings from the plugins page.
-add_filter(
-	'plugin_action_links_' . plugin_basename( __FILE__ ),
-	function ( $links ) {
-		$links[] = '<a href="' . admin_url( 'options-general.php?page=chatrix' ) . '">' . __( 'Settings' ) . '</a>';
-
-		return $links;
-	}
-);
-
 if ( is_admin() ) {
+	// Link to the plugin settings from the plugins page.
+	add_filter(
+		'plugin_action_links_' . plugin_basename( __FILE__ ),
+		function ( $links ) {
+			$links[] = '<a href="' . admin_url( 'options-general.php?page=chatrix' ) . '">' . __( 'Settings' ) . '</a>';
+
+			return $links;
+		}
+	);
+
 	adminMain();
 }
 
