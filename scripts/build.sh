35a7901123bfac949e53f4d8152f3dcd391ffc68
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-14 14:09:00 +0100

    Remove frontend

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# Builds both chatrix itself and the wordpress plugin.
-
-# Build chatrix
-yarn install
-yarn build
-
 # Generate wordpress plugin's readme.txt
 php wordpress/chatrix/bin/transform-readme.php
 
