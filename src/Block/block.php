7250206a73d49c981f7e7928b2acf135f3d7b4d2
Paulo Pinto <paulo.pinto@automattic.com>
2022-10-03 13:08:37 +0100

    Add site status test to make sure block.json exists

--- src/Block/block.php
+++ src/Block/block.php
@@ -5,6 +5,9 @@ namespace Automattic\Chatrix\Block;
 function register() {
 	$block_path      = dirname( plugin_dir_path( __FILE__ ), 2 ) . '/build-block';
 	$block_json_path = "$block_path/block.json";
+
+	register_site_status_test( $block_json_path );
+
 	if ( ! file_exists( $block_json_path ) ) {
 		return;
 	}
@@ -35,4 +38,42 @@ function render() {
 	</div>
 	<?php
 	return ob_get_clean();
+}
+
+function register_site_status_test( string $block_json_path ) {
+	if ( file_exists( $block_json_path ) ) {
+		$label  = __( 'The block.json file exists', 'chatrix' );
+		$status = 'good';
+		$badge  = 'green';
+	} else {
+		$label  = __( 'The block.json file does not exist', 'chatrix' );
+		$status = 'critical';
+		$badge  = 'red';
+	}
+
+	add_filter(
+		'site_status_tests',
+		function ( array $tests ) use ( $label, $status, $badge ) {
+			$tests['direct']['chatrix-block-json'] = array(
+				'label' => __( 'The block.json file exists', 'chatrix' ),
+				'test'  => function () use ( $label, $status, $badge ) {
+					return array(
+						'label'       => wp_kses_post( $label ),
+						'status'      => $status,
+						'badge'       => array(
+							'label' => __( 'Chatrix', 'chatrix' ),
+							'color' => $badge,
+						),
+						'description' =>
+							'<p>' .
+							__( 'If a block.json file is not found under wp-content/plugins/chatrix/build-block/block.json, the Gutenberg block will not be available.', 'chatrix' ) .
+							'</p>',
+						'test'        => 'chatrix-block-json',
+					);
+				},
+			);
+
+			return $tests;
+		}
+	);
 }
