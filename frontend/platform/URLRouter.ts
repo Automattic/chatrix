87e1319d5b39f31d345d3bfb78172da82cf83f6b
Paulo Pinto <paulo.pinto@automattic.com>
2022-10-11 15:12:53 +0100

    Make sure loginToken is removed from both the iframe and the parent URL

--- frontend/platform/URLRouter.ts
+++ frontend/platform/URLRouter.ts
@@ -1,19 +1,43 @@
 import { SegmentType } from "hydrogen-web/src/domain/navigation";
+import { Navigation, Path, Segment } from "hydrogen-web/src/domain/navigation/Navigation";
 import { URLRouter as BaseURLRouter } from "hydrogen-web/src/domain/navigation/URLRouter";
+import { History } from "./History";
+
+type ParseURLPath<SegmentType> = (urlPath: string, currentNavPath: Path<SegmentType>, defaultSessionId?: string) => Segment<SegmentType>[];
+type StringifyPath<SegmentType> = (path: Path<SegmentType>) => string;
 
 export class URLRouter extends BaseURLRouter<SegmentType> {
+    private _hist: History;
+
+    constructor(history: History, navigation: Navigation<SegmentType>, parseUrlPath: ParseURLPath<SegmentType>, stringifyPath: StringifyPath<SegmentType>) {
+        super(history, navigation, parseUrlPath, stringifyPath);
+        this._hist = history;
+    }
+
     createSSOCallbackURL(): string {
         // The URL of the iframe's parent.
         return parent.location.href;
     }
 
     normalizeUrl(): void {
-        // Remove query params from iframe URL.
-        super.normalizeUrl();
+        // Remove loginToken query parameter from the iframe's URL.
+        let iframeUrl = this.removeLoginToken(window.location.pathname, window.location.search, window.location.hash);
+        this._hist.replaceUrlSilently(iframeUrl);
 
-        // Remove loginToken query param from iframe parent's URL.
-        const urlParams = new URLSearchParams(parent.location.search);
+        // Remove loginToken query parameter from the iframe's parent's URL.
+        let parentUrl = this.removeLoginToken(parent.location.pathname, parent.location.search, parent.location.hash);
+        parent.history.replaceState(null, '', parentUrl);
+    }
+
+    private removeLoginToken(path: string, search: string, hash: string): string {
+        const urlParams = new URLSearchParams(search);
         urlParams.delete("loginToken");
-        parent.history.replaceState(null, '', (urlParams.entries.length ? '?' + urlParams : './') + location.hash);
+
+        let url = path;
+        if (urlParams.toString() !== "") {
+            url += "?" + urlParams.toString();
+        }
+
+        return url + hash;
     }
 }
