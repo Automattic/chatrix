ac4f1a7a9432cfe20d041a94d7d0986c3d9f75a0
Paulo Pinto <paulo.pinto@automattic.com>
2022-10-17 18:01:14 +0100

    Render iframe in javascript instead of ServerSideRender

--- block/edit.tsx
+++ block/edit.tsx
@@ -1,21 +1,38 @@
 import { useBlockProps } from "@wordpress/block-editor";
 import { WPElement } from '@wordpress/element';
-import ServerSideRender from "@wordpress/server-side-render";
+import { addQueryArgs } from "@wordpress/url";
 import './editor.scss';
 import Inspector from "./inspector";
 
+// Config coming from PHP.
+type BlockConfig = {
+    iframeUrl: string,
+}
+
+type Window = {
+    chatrix_block_config: BlockConfig,
+} & typeof window;
+
 export default function Edit({ attributes, setAttributes }): WPElement {
     const blockProps = useBlockProps();
+    const win: Window = window as Window;
+    const config: BlockConfig = win.chatrix_block_config;
+    const iframeTitle = `${blockProps["data-title"]} iframe`;
+
+    const iframeSrc = addQueryArgs(config.iframeUrl, {
+        defaultHomeserver: attributes.defaultHomeserver,
+    });
+
     return (
         <div {...blockProps}>
             <Inspector
                 attributes={attributes}
                 setAttributes={setAttributes}
             />
-            <ServerSideRender
-                block="automattic/chatrix"
-                attributes={attributes}
-            />
+            <iframe className="wp-block-automattic-chatrix-iframe"
+                    title={iframeTitle}
+                    src={iframeSrc}
+            ></iframe>
         </div>
     );
 }
