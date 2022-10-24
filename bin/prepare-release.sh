2ec6028ded795d6987b451f6c2fbdea4600ecc2b
Paulo Pinto <paulo.pinto@automattic.com>
2022-10-24 14:57:48 +0100

    Add script to prepare a release
    Only sets the version for now.

---
+++ bin/prepare-release.sh
@@ -0,0 +1,20 @@
+set -e
+
+if [ -z "$1" ]; then
+    echo "Provide a new version, current version is $(jq '.version' package.json)"
+    exit 1
+fi
+
+VERSION=$1
+
+for file in package.json composer.json block/block.json
+do
+  jq ".version = \"$VERSION\"" $file > $file.tmp
+  mv $file.tmp $file
+  git add $file
+done
+
+sed -i"" -e "s/\(Version: \)\(.*\)/\1$VERSION/g" chatrix.php
+sed -i"" -e "s/\(AUTOMATTIC_CHATRIX_VERSION = '\)\(.*\)/\1$VERSION';/g" chatrix.php
+rm -f chatrix.php-e
+git add chatrix.php
