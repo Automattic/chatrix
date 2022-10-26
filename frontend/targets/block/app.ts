a1adee28174a615ead2f8c5c395c174d946bfdb5
Paulo Pinto <paulo.pinto@automattic.com>
2022-10-26 16:58:23 +0100

    Inline app.ts into index.html

--- frontend/targets/block/app.ts
+++
@@ -1,6 +0,0 @@
-import { Main } from "../../main";
-
-const main = new Main("chatrix-block");
-main.start().catch((error) => {
-    throw new Error("Failed to start app: " + error);
-});
--- frontend/targets/popup/app.ts
+++
@@ -1,6 +0,0 @@
-import { Main } from "../../main";
-
-const main = new Main("chatrix-popup");
-main.start().catch((error) => {
-    throw new Error("Failed to start app: " + error);
-});
