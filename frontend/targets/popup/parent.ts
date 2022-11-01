668239437fca0a98c89db9870cab264dd8ae8d3c
Paulo Pinto <paulo.pinto@automattic.com>
2022-11-01 14:34:14 +0000

    Load iframe when button is clicked

--- frontend/targets/popup/parent.ts
+++ frontend/targets/popup/parent.ts
@@ -1,4 +1,23 @@
-import { IframeParams } from "../../parent/iframe";
+import { Iframe, IframeParams } from "../../parent/iframe";
+import { StartButton } from "./button";
 
-export function loadPopup(containerId: string, hostRoot: string, params: IframeParams) {
+(window as any).AutomatticChatrixPopup = {
+    iframe: null,
+};
+
+export function loadStartButton(containerId: string, hostRoot: string, params: IframeParams): void {
+    const button = new StartButton(() => {
+        if (!isIframeLoaded()) {
+            const iframe = new Iframe(hostRoot, params);
+            iframe.mount(containerId);
+            (window as any).AutomatticChatrixPopup.iframe = iframe;
+        } else {
+        }
+    });
+    button.mount(containerId);
+}
+
+
+function isIframeLoaded(): boolean {
+    return (window as any).AutomatticChatrixPopup.iframe !== null;
 }
