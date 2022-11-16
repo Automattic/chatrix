810cb6fc12bd0f18a2b7f415feb214573fcf5a96
Paulo Pinto <paulo.pinto@automattic.com>
2022-11-16 15:17:46 +0000

    Create artifacts

--- bin/create-release-artifacts.sh
+++ bin/create-release-artifacts.sh
@@ -20,7 +20,8 @@ if [[ $VERSION == v* ]]; then
   VERSION="${VERSION:1}"
 fi
 
-RELEASE_DIR="$(pwd)/release/$VERSION"
+RELEASE_ROOT_DIR="$(pwd)/release"
+RELEASE_DIR="$RELEASE_ROOT_DIR/$VERSION"
 rm -rf "$RELEASE_DIR" && mkdir -p "$RELEASE_DIR"
 
 yarn install
@@ -34,9 +35,18 @@ PATHS_TO_INCLUDE=(
 "README.md"
 )
 for path in "${PATHS_TO_INCLUDE[@]}";do
-    cp -r "$path" "$RELEASE_DIR/$path"
+  cp -r "$path" "$RELEASE_DIR/$path"
 done
 
 rm -rf "$RELEASE_DIR/build/.tmp"
 
 COMPOSER_VENDOR_DIR="$RELEASE_DIR/vendor" composer install --no-ansi --no-dev --no-interaction --no-plugins --no-scripts --optimize-autoloader
+
+# Rename release directory from version name (e.g. 1.2.3) to `chatrix` so that root directory in the artifacts is named `chatrix`.
+# Then create the archives, and rename back to the versioned name (e.g. 1.2.3).
+rm -rf "$RELEASE_ROOT_DIR/chatrix"
+mv "$RELEASE_DIR" "$RELEASE_ROOT_DIR/chatrix"
+cd "$RELEASE_ROOT_DIR"
+zip -r "chatrix-$VERSION.zip" chatrix
+tar -cvzf "chatrix-$VERSION.tar.gz" chatrix
+mv "$RELEASE_ROOT_DIR/chatrix" "$RELEASE_DIR"
