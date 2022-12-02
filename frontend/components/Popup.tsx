7d584268f52918d8cedf6058e01362f1bd14f2f8
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-02 17:51:50 +0000

    Add Popup component

---
+++ frontend/components/Popup.tsx
@@ -0,0 +1,22 @@
+import { useState } from '@wordpress/element';
+import { Chat, ChatProps } from "./Chat";
+
+export interface Props extends ChatProps {
+}
+
+export function Popup(props: Props) {
+    const [active, setActive] = useState(false);
+    const chat = active ? <Chat {...props}/> : undefined;
+
+    return (
+        <div className={"chatrix-component-popup"}>
+            {chat}
+            <div className={"chatrix-component-popup-start-button"}>
+                <button
+                    className={`${active ? "active" : ""}`}
+                    onClick={() => setActive(prevActive => !prevActive)}
+                ></button>
+            </div>
+        </div>
+    );
+}
