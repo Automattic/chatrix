ae93cf814048d46f1c6153cab79abeaebabc97f0
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-02 17:52:06 +0000

    Use Popup component for popup dev env

--- frontend/targets/popup/parent.ts
+++ frontend/targets/popup/parent.ts
@@ -1,13 +1,21 @@
+import { createElement, render } from '@wordpress/element';
+import { ChatProps } from "../../components/Chat";
+import { Popup } from "../../components/Popup";
 import { IframeParams } from "../../parent/iframe";
 import "./parent.css";
-import { Popup } from "./src/popup";
+import { Popup as PopupLegacy } from "./src/popup";
+
+export function init(containerId: string, props: ChatProps) {
+    const element = createElement(Popup, props);
+    render(element, document.getElementById(containerId));
+}
 
 export function loadPopup(containerId: string, hostRoot: string, params: IframeParams): void {
-    window.AutomatticChatrixPopup = new Popup(containerId, hostRoot, params);
+    window.AutomatticChatrixPopup = new PopupLegacy(containerId, hostRoot, params);
 }
 
 declare global {
     interface Window {
-        AutomatticChatrixPopup: Popup;
+        AutomatticChatrixPopup: PopupLegacy;
     }
 }
