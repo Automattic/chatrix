146f53167ec276f44f53927b179d4c152b9f6b5b
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-09 12:19:37 +0000

    No longer need a render callback in PHP

--- src/Block/block.php
+++ src/Block/block.php
@@ -15,56 +15,9 @@ function register() {
 	add_action(
 		'init',
 		function () use ( $block_json_path ) {
-			$metadata = parse_block_json( $block_json_path );
-			register_block_type(
-				$block_json_path,
-				array(
-					'render_callback' => 'Automattic\Chatrix\Block\render',
-					'attributes'      => $metadata['attributes'],
-				)
-			);
+			register_block_type( $block_json_path );
 		}
 	);
-}
-
-function render( array $attributes ): string {
-	$handle       = 'chatrix-block-config';
-	$container_id = 'wp-block-automattic-chatrix-container';
-
-	$json_data = wp_json_encode(
-		array(
-			'containerId' => $container_id,
-			'attributes'  => $attributes,
-		)
-	);
-
-	wp_register_script( $handle, '', array(), automattic_chatrix_version(), true );
-	wp_add_inline_script( $handle, "window.ChatrixBlockConfig = $json_data;", 'before' );
-	wp_enqueue_script( $handle );
-
-	return render_markup( $container_id, $attributes );
-}
-
-function render_markup( string $container_id, array $attributes ): string {
-	ob_start();
-	?>
-	<div <?php echo wp_kses_data( get_block_wrapper_attributes() ); ?> id="<?php echo esc_attr( $container_id ); ?>">
-		<?php // Iframe will be rendered here. ?>
-	</div>
-	<?php
-	return ob_get_clean();
-}
-
-
-function parse_block_json( string $block_json_path ): array {
-	// phpcs discourages file_get_contents for remote URLs, and recommends using wp_remote_get().
-	// However, here we're dealing with a path to a file on disk, so we ignore phpcs's warning.
-	// This is possibly a bug in phpcs, which seems to have code to check if the path is remote, but fails in this case.
-	// For more info see https://github.com/WordPress/WordPress-Coding-Standards/issues/943.
-	// phpcs:ignore
-	$block_json_contents = file_get_contents( $block_json_path );
-
-	return json_decode( $block_json_contents, true );
 }
 
 function register_site_status_test( string $block_json_path ) {
