b500ce35ff1cbe6334e114ae52bf9279c600a56e
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-09 15:40:18 +0000

    Use a data attribute per block attribute

--- frontend/block/save.tsx
+++ frontend/block/save.tsx
@@ -1,10 +1,26 @@
 import { useBlockProps } from "@wordpress/block-editor";
+import { parseAttributes } from "../components/block";
 
-export default function Save({ attributes }) {
-    const props = {
-        ...useBlockProps.save(),
-        "data-attributes": encodeURIComponent(JSON.stringify(attributes)),
+interface Props {
+    attributes: object,
+}
+
+export default function Save(props: Props) {
+    const attributes = parseAttributes(props.attributes);
+
+    const blockProps = {
+        "data-default-homeserver": encodeURIComponent(attributes.defaultHomeserver ?? ""),
+        "data-room-id": encodeURIComponent(attributes.roomId ?? ""),
+        "data-height": encodeURIComponent(attributes.height ? attributes.height.toString() : ''),
+        "data-border-width": encodeURIComponent(attributes.borderWidth ? attributes.borderWidth.toString() : ''),
+        "data-border-radius": encodeURIComponent(attributes.borderRadius ? attributes.borderRadius.toString() : ''),
+        "data-border-style": encodeURIComponent(attributes.borderStyle ?? ''),
+        "data-border-color": encodeURIComponent(attributes.borderColor ?? ''),
     };
 
-    return <div {...props}/>;
+    return (
+        <div {...useBlockProps.save()}>
+            <span {...blockProps} hidden></span>
+        </div>
+    );
 }
