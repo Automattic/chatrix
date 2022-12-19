5ea5d3dcdadb00e2fa0b31cd8c96f0c45b055268
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-19 15:26:49 +0000

    Set a cookie when user logs out

--- src/Sessions/logout.php
+++ src/Sessions/logout.php
@@ -3,6 +3,16 @@
 namespace Automattic\Chatrix\Sessions;
 
 function init_logout() {
+	// Set a cookie when user logs out.
+	add_action(
+		'wp_logout',
+		function () {
+			// Expire in 10 minutes.
+			$expiration = time() + ( 10 * 60 );
+			setcookie( 'chatrix-logout', 'true', $expiration, COOKIEPATH, COOKIE_DOMAIN );
+		}
+	);
+
 	// Log out user from Chatrix on non-logged in page load.
 	delete_sessions();
 }
