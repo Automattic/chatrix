97a24f56cfcb3d1208a843e7292e5bc1bf85877e
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-19 15:50:33 +0000

    Enqueue logout script when user is not logged in

--- src/plugin.php
+++ src/plugin.php
@@ -8,6 +8,7 @@ use function Automattic\Chatrix\Sessions\init_logout;
 
 const SCRIPT_HANDLE_CONFIG     = 'chatrix-config';
 const SCRIPT_HANDLE_APP        = 'chatrix-app';
+const SCRIPT_HANDLE_LOGOUT     = 'chatrix-logout';
 const CONFIG_VARIABLE          = 'ChatrixConfig';
 
 function main() {
@@ -32,7 +33,7 @@ function register_scripts() {
 			wp_enqueue_script( SCRIPT_HANDLE_CONFIG );
 			wp_add_inline_script( SCRIPT_HANDLE_CONFIG, 'window.' . CONFIG_VARIABLE . " = $json_data;" );
 
-			// Note we don't enqueue the script yet. It will be enqueue whenever SCRIPT_HANDLE_APP
+			// Note we don't enqueue the SCRIPT_HANDLE_APP script yet. It will be enqueued whenever SCRIPT_HANDLE_APP
 			// is specified as a dependency of another script.
 			wp_register_script(
 				SCRIPT_HANDLE_APP,
@@ -41,6 +42,17 @@ function register_scripts() {
 				automattic_chatrix_version(),
 				true
 			);
+
+			if ( ! is_user_logged_in() ) {
+				wp_register_script(
+					SCRIPT_HANDLE_LOGOUT,
+					root_url() . '/logout.iife.js',
+					array(),
+					automattic_chatrix_version(),
+					false
+				);
+				wp_enqueue_script( SCRIPT_HANDLE_LOGOUT );
+			}
 		}
 	);
 }
