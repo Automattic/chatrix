4f293ac86f36e3e0d3deb586eb48b691027dbfdc
Paulo Pinto <paulo.pinto@automattic.com>
2022-12-19 16:09:41 +0000

    Move error logging to function

--- frontend/logout.ts
+++ frontend/logout.ts
@@ -1,12 +1,3 @@
-async function chatrixLogoutSession(session) {
-    return fetch(session.homeserver + '/_matrix/client/v3/logout', {
-        method: 'POST',
-        headers: {
-            'Authorization': 'Bearer ' + session.accessToken,
-        },
-    });
-}
-
 window.addEventListener('DOMContentLoaded', () => {
     for (const [key, value] of Object.entries(localStorage)) {
         if (!key.startsWith('chatrix_sessions')) {
@@ -19,10 +10,23 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
         let session = sessions[0];
-        this.chatrixLogoutSession(session).then(() => {
+        void logoutSession(session).then(() => {
             localStorage.removeItem(key);
-        }).catch((error) => {
-            console.log(`Failed to logout chatrix session. deviceId: ${session.deviceId}`, error);
         });
     }
 });
+
+async function logoutSession(session) {
+    let promise = fetch(session.homeserver + '/_matrix/client/v3/logout', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + session.accessToken,
+        },
+    });
+
+    promise.catch(error => {
+        console.log(`Failed to logout chatrix session. deviceId: ${session.deviceId}`, error);
+    });
+
+    return promise;
+}
