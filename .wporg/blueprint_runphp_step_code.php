0aac179c1d99d362a7e2fad724b5edd8c2043a81
ashfame <mail@ashfame.com>
2024-01-19 23:23:33 +0400

    improve blueprint

--- .wporg/blueprint_runphp_step_code.php
+++ .wporg/blueprint_runphp_step_code.php
@@ -2,7 +2,7 @@
 
 require_once 'wordpress/wp-load.php';
 
-$chatrix_block_simple = '<!-- wp:paragraph --> <p>Below you can see "Chatrix" block in action. You can add it anywhere using either <code>/chatrix</code> or by looking up chatrix when choosing which block to insert.</p> <!-- /wp:paragraph -->  <!-- wp:paragraph --> <p>Even though its not functional in this Playground demo, since it requires its own service worker and that is not compatible with the Playground (running WordPress in your browser) at the moment, you can at least see how it looks and it can be added anywhere you can add a Gutenberg block :)</p> <!-- /wp:paragraph --> <!-- wp:automattic/chatrix {"enableServiceWorker":false,"instanceId":"2530674218113325","defaultHomeserver":"matrix.org"} /-->';
+$chatrix_block_simple = '<!-- wp:paragraph --> <p>Below you can see "Chatrix" block in action. You can add it anywhere using either <code>/chatrix</code> or by looking up chatrix when choosing which block to insert.</p> <!-- /wp:paragraph --> <!-- wp:automattic/chatrix {"enableServiceWorker":false,"instanceId":"2530674218113325","defaultHomeserver":"matrix.org"} /--> <!-- wp:paragraph --> <p>Chatrix can also run in <a href="/">single room mode</a>.</p> <!-- /wp:paragraph -->';
 
 $chatrix_block_2col = <<<CHATRIXBLOCK2COLUMNS
 <!-- wp:paragraph -->
@@ -14,21 +14,20 @@ $chatrix_block_2col = <<<CHATRIXBLOCK2COLUMNS
 <!-- wp:column {"width":"33.33%"} -->
 <div class="wp-block-column" style="flex-basis:33.33%">
 <!-- wp:paragraph -->
-<p>Here can be some login instructions of participating in the chat window on the right side:</p>
+<p>Edit this page to specify what room should Chatrix run in as, single-room mode. By default it loads up <pre>#matrix:matrix.org</pre></p>
+<!-- /wp:paragraph -->
+<!-- wp:paragraph -->
+<p>You would need to select Chatrix block to reveal its settings on the right side panel.</p>
 <!-- /wp:paragraph -->
 </div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"66.66%"} -->
 <div class="wp-block-column" style="flex-basis:66.66%">
-<!-- wp:automattic/chatrix {"enableServiceWorker":false,"instanceId":"4986644425212020","defaultHomeserver":"matrix.org"} /-->
+<!-- wp:automattic/chatrix {"enableServiceWorker":false,"instanceId":"4986644425212020","roomId":"#matrix:matrix.org","defaultHomeserver":"matrix.org"} /-->
 </div>
 <!-- /wp:column -->
 </div>
 <!-- /wp:columns -->
-
-<!-- wp:paragraph -->
-<p>Unfortunately, it's not possible to login into Matrix inside of Playground demo, since it requires its own service worker and that is not compatible with the Playground at the moment. You can explore how it looks and how it can be added anywhere you can add a Gutenberg block :)</p>
-<!-- /wp:paragraph -->
 CHATRIXBLOCK2COLUMNS;
 
 $sample_page_post_id = 2;
@@ -36,12 +35,14 @@ $sample_page_post_id = 2;
 wp_update_post( array(
     'ID' => $sample_page_post_id,
     'post_title' => 'Chatrix [Regular]',
+    'post_name' => 'chatrix-regular-mode',
     'post_content' => $chatrix_block_simple
 ) );
 
 wp_insert_post( array(
     'ID' => 3,
-    'post_title' => 'Chatrix [2 Column]',
+    'post_title' => 'Chatrix [Single Room Mode]',
+    'post_name' => 'chatrix-single-room-mode',
     'post_type' => 'page',
     'post_status' => 'publish',
     'post_content' => $chatrix_block_2col
@@ -50,6 +51,7 @@ wp_insert_post( array(
 wp_insert_post( array(
     'ID' => 4,
     'post_title' => 'Chatrix [Popup]',
+    'post_name' => 'chatrix-in-popup-mode',
     'post_type' => 'page',
     'post_status' => 'publish',
     'post_content' => '<!-- wp:paragraph --> <p>On this page, you can see the popup widget at the bottom right of your screen. Click on it to show Chatrix.</p> <!-- /wp:paragraph -->'
